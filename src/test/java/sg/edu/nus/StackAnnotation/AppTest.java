da55a19b397c5b2157d5fcc6c9863d5f6b6201aa
Justin Hu <justin.hu.rj@gmail.com>
2021-06-29 15:17:28 +0800

    fail Junit Test 2

--- src/test/java/sg/edu/nus/StackAnnotation/AppTest.java
+++ src/test/java/sg/edu/nus/StackAnnotation/AppTest.java
@@ -15,8 +15,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        MyStringStack stack = new MyStringStack();
-		stack.push (s1);
-		assertTrue (stack.isEmpty());
+        assertTrue(false)
     }
 }
